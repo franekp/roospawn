f7ad093b4c19970e9b5f32c47aaa0a76ea0bf571
Franciszek Piszcz <franek.piszcz@gmail.com>
2025-02-25 01:52:14 +0100

    Add notebook code, untested

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,71 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TextDecoder, TextEncoder } from 'util';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "taskdozer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('taskdozer.helloWorld', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		console.log('Hello World from TaskDozer!');
-
-		let ai_extension = vscode.extensions.getExtension('rooveterinaryinc.roo-cline');
-		if (!ai_extension) {
-			throw new Error('taskdozer: roo-cline extension not found');
-		}
-		let ai_api = ai_extension.exports;
-		await ai_api.startNewTask("Write a function that calculates factorial in TypeScript");
-		//await ai_api.sendMessage("I need to write a function that calculates factorial in TypeScript");
-		
-		console.log('Run the query!');
-	});
-
-	let ai_extension = vscode.extensions.getExtension('rooveterinaryinc.roo-cline');
-	console.log(ai_extension);
-
-	context.subscriptions.push(disposable);
+interface TaskDozerCell {
+    kind: 'markdown' | 'code';
+    value: string;
+    language: string;
 }
 
-// This method is called when your extension is deactivated
+interface TaskDozerNotebook {
+    cells: TaskDozerCell[];
+}
+
+class TaskDozerSerializer implements vscode.NotebookSerializer {
+    async deserializeNotebook(content: Uint8Array, _token: vscode.CancellationToken): Promise<vscode.NotebookData> {
+        const contents = new TextDecoder().decode(content);
+
+        let raw: TaskDozerNotebook;
+        try {
+            raw = JSON.parse(contents);
+        } catch {
+            raw = { cells: [] };
+        }
+
+        const cells = raw.cells.map(item => new vscode.NotebookCellData(
+            item.kind === 'code' ? vscode.NotebookCellKind.Code : vscode.NotebookCellKind.Markup,
+            item.value,
+            item.language
+        ));
+
+        return new vscode.NotebookData(cells);
+    }
+
+    async serializeNotebook(data: vscode.NotebookData, _token: vscode.CancellationToken): Promise<Uint8Array> {
+        const cells: TaskDozerCell[] = data.cells.map(cell => ({
+            kind: cell.kind === vscode.NotebookCellKind.Code ? 'code' : 'markdown',
+            value: cell.value,
+            language: cell.languageId
+        }));
+
+        const notebook: TaskDozerNotebook = { cells };
+        return new TextEncoder().encode(JSON.stringify(notebook, null, 2));
+    }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('TaskDozer extension is now active!');
+
+    // Register notebook serializer
+    context.subscriptions.push(
+        vscode.workspace.registerNotebookSerializer('taskdozer', new TaskDozerSerializer())
+    );
+
+    // Register hello world command
+    const disposable = vscode.commands.registerCommand('taskdozer.helloWorld', async () => {
+        console.log('Hello World from TaskDozer!');
+
+        let ai_extension = vscode.extensions.getExtension('rooveterinaryinc.roo-cline');
+        if (!ai_extension) {
+            throw new Error('taskdozer: roo-cline extension not found');
+        }
+        let ai_api = ai_extension.exports;
+        await ai_api.startNewTask("Write a function that calculates factorial in TypeScript");
+        
+        console.log('Run the query!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
