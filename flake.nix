51decc2bc6e284a2a0e404a9fd260274fa40fd3e
Wojciech Kordalski <wkordalski@pm.me>
2025-04-07 18:21:25 +0200

    Install VSCE i Posthog-node

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
           inherit system;
         };
         
-        nativeBuildInputs = with pkgs; [ yarn nodejs_23 ];
+        nativeBuildInputs = with pkgs; [ yarn nodejs_20 vsce ];
         buildInputs = with pkgs; [];
         devInputs = with pkgs; [];
 
@@ -22,7 +22,7 @@
         vscodeTestFhs = pkgs.buildFHSEnv {
           name = "vscode-test-env";
           targetPkgs = pkgs: (with pkgs; [
-            yarn nodejs_23 glib nss nspr dbus atk gtk3 pango cairo
+            yarn nodejs_20 glib nss nspr dbus atk gtk3 pango cairo
             xorg.libX11 xorg.libXcomposite xorg.libXdamage xorg.libXext xorg.libXfixes xorg.libXrandr
             libgbm expat xorg.libxcb libxkbcommon udev alsa-lib libGL
           ]);
